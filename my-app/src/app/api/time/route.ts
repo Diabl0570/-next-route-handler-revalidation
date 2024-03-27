0524cfdcb49ae7526e7ad0d733801aa8b90877b7
Kasper Sprengers <diabl0570@hotmail.com>
2024-03-27 15:41:18 +0100

    no edge

--- my-app/src/app/api/time/route.ts
+++ my-app/src/app/api/time/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getTime } from "@/app/services/getTime";
 
-export const runtime = "edge";
-
 export async function GET(request: NextRequest) {
   const data = await getTime();
 
