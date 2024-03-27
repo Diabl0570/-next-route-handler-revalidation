818afa582a90ace216d3b0446ee426f9563e8530
Kasper Sprengers <diabl0570@hotmail.com>
2024-03-27 15:58:03 +0100

    revalidate 10 sec

--- my-app/src/app/services/getTime.ts
+++ my-app/src/app/services/getTime.ts
@@ -1,6 +1,6 @@
 export const getTime = async () => {
     const time = await fetch("https://worldtimeapi.org/api/ip", {
-      next: { tags: ["time-tag"]},
+      next: { tags: ["time-tag"], revalidate: 10},
     });
     return time.json();
   };
