a5d48836e7d7828e2a88343466fe66e3a7c3ced3
Kasper Sprengers <diabl0570@hotmail.com>
2024-03-27 12:37:16 +0100

    add endpoint for time

---
+++ my-app/src/app/api/time/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const getTime = async () => {
+  const time = await fetch("https://worldtimeapi.org/api/ip", {
+    next: { revalidate: 1 },
+  });
+  return time.json();
+};
+
+export async function GET(request: NextRequest) {
+  const data = await getTime();
+
+  if (!data) {
+    return NextResponse.json([]);
+  }
+
+  return NextResponse.json(data, {
+    headers: {
+      "Cache-Control": "public, s-maxage=10",
+      // "CDN-Cache-Control": "public, s-maxage=3600",
+      // "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+    },
+  });
+}
